4e041cfcaca0213bec7b02bf6f1aa4c28005d6a4
Lucas Antunes <lucasdanielantunes@gmail.com>
2019-12-13 15:44:28 -0300

    Crud Estado Finalizado

--- store-app/src/app/components/admin/estado/estado-form/estado-form.component.ts
+++ store-app/src/app/components/admin/estado/estado-form/estado-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Estado } from 'src/app/models/estado.model';
+import { Validators, FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
+import { EstadoService } from 'src/app/services/estado.service';
 
 @Component({
   selector: 'app-estado-form',
@@ -7,9 +11,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadoFormComponent implements OnInit {
 
-  constructor() { }
+  dataAtual = new Date();
+  title: string;
+  edit: string;
+
+  formRegister = this.fb.group({
+    id: undefined,
+    estado: ['', [Validators.required]],
+    dataatualizacao: ['', []]
+  });
+
+  constructor(
+    public dialogRef: MatDialogRef<EstadoFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public estado: Estado,
+
+    private fb: FormBuilder,
+    private estadoService: EstadoService,
+    private snackBar: MatSnackBar,
+  ) {
+    if (this.estado != null) {
+      this.formRegister.setValue(estado);
+      this.title = 'Atualização Cadastro Estado';
+      this.edit = 'Atualizar'
+    } else {
+      this.title = 'Novo Cadastro Estado';
+      this.edit = 'Salvar'
+    }
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    let estado: Estado = this.formRegister.value;
+    if (!estado.id) {
+      this.addEstado(estado);
+    } else {
+      this.updateEstado(estado);
+    }
+  }
+
+  addEstado(estado: Estado) {
+    estado.dataatualizacao = this.dataAtual;
+    this.estadoService.createOrUpdate(estado)
+      .then(() => {
+        this.snackBar.open('Cadastro realizado com sucesso!', 'OK', { duration: 2000 });
+        this.cancelar();
+      })
+      .catch(() => {
+        this.snackBar.open('Erro ao realizar cadastro', 'OK', { duration: 2000 });
+      })
+  }
+
+  updateEstado(estado: Estado) {
+    estado.dataatualizacao = this.dataAtual;
+    this.estadoService.createOrUpdate(estado)
+      .then(() => {
+        this.snackBar.open('Cadastro atualizado sucesso!', 'OK', { duration: 2000 });
+        this.cancelar();
+      })
+      .catch((e) => {
+        this.snackBar.open('Erro ao atualizar cadastro!', 'OK', { duration: 2000 });
+      })
+  }
+
+  cancelar(): void {
+    this.dialogRef.close();
   }
 
 }
--- store-app/src/app/components/admin/estado/estado.component.ts
+++ store-app/src/app/components/admin/estado/estado.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Estado } from 'src/app/models/estado.model';
+import { Observable, Subject } from 'rxjs';
+import { EstadoService } from 'src/app/services/estado.service';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { takeUntil } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { EstadoFormComponent } from './estado-form/estado-form.component';
 
 @Component({
   selector: 'app-estado',
@@ -7,9 +14,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadoComponent implements OnInit {
 
-  constructor() { }
+  estados$: Observable<Estado[]>;
+  displayedColumns = ['estado', 'dataatualizacao', 'operations'];
+  estados: Estado[] = [];
+ 
+
+  private unsubscribe$: Subject<any> = new Subject<any>();
+
+  @ViewChild('estado', { static: false }) atorNome: ElementRef;
+
+  constructor(
+    private estadoService: EstadoService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) { }
+
 
   ngOnInit() {
+    this.estadoService.list()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((estado) => this.estados = estado);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+  }
+
+  del(estado: Estado){
+    Swal.fire({
+      title: 'Confirma a exclusão do estado?',
+      text: "",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    }).then((result) => {
+      if(result.value) {
+        this.estadoService.delete(estado.id)
+          .then(() => {
+            this.snackBar.open('Estado excluido com sucesso!', 'OK', { duration: 2000 });
+          })
+      }
+    })
+  }
+
+  novoCadastro(){
+    this.dialog.open(EstadoFormComponent, {width: '400px'});
+  }
+
+  edit(d: Estado){
+    this.dialog.open(EstadoFormComponent, { width: '400px', data: d});
   }
 
 }
